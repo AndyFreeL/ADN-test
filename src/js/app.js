89555667b25cc37f0d55bbe5676c6662b9d6d45d
AndyFreeL <83203565+AndyFreeL@users.noreply.github.com>
2022-05-28 22:42:14 +0700

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as functions from './modules/functions.js';
+import form from './modules/form.js';
+
+functions.isWebp();
+functions.filterSelection('all')
+
+console.log('Адаптив кривой))');
