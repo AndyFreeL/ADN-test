264f44724069880f513a82eff3fe243e64d51697
AndyFreeL <83203565+AndyFreeL@users.noreply.github.com>
2022-05-28 23:11:49 +0700

    upload

--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-import * as functions from './modules/functions.js';
-import form from './modules/form.js';
-
-functions.isWebp();
-functions.filterSelection('all')
-
-console.log('Адаптив кривой))');
